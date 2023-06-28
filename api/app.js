cd80cdf3cab606a4f2870700e682269bc7626202
Djonatan Willenz <DjonatanHorus@Outlook.com>
2023-06-28 17:52:48 -0300

    ajustes

--- api/app.js
+++ api/app.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.status(200).json({ message: "Server online" }));
 const conn = require('./src/db/connDB');
 conn();
 
--- ws/src/db/coonDB.js
+++ ws/src/db/coonDB.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 async function db() {
     try {
         mongoose.set("strictQuery", true);
-        await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@app.bjknbjj.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`);
+        await mongoose.connect(`mongodb://db/app`);
         console.log("Conectado com sucesso ao banco de dados.")
     } catch (error) {
-        console.log('Erro ao conectar DB...')
+        console.log('Erro ao conectar DB...', error.message)
     }
 }
 
